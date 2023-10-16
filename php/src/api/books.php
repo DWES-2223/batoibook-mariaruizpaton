2248842464cd0cceb4aaadb6f53f8ba95bd5eab3
María Ruiz <mrp990305@gmail.com>
2023-10-16 19:25:12 +0200

    commit pt2 16-10

---
+++ php/src/api/books.php
@@ -0,0 +1,28 @@
+<?php
+
+use BatBook\Book;
+
+$id = isset($_GET['id']) ? $_GET['id'] : null;
+$books = [
+    1 => new Book(0, "0012", 'Publisher Name', 19.99, 300, 'Available', 'book.jpg', 'This is a great book')
+];
+
+if ($id === null) {
+    throw new NotFoundException("no existe id");
+} else {
+    try {
+        if (array_key_exists($id, $books)){
+            echo json_encode($books[$id]->toJSON());
+        } else {
+            throw new NotFoundException("Id ${$id} no encontrado");
+        }
+    } catch (NotFoundException $e){
+        echo "Error " . $e->getMessage() . "\n";
+    }
+}
+
+
+
+class NotFoundException extends Exception {
+
+}
